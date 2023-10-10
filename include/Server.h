da21927ab3fe9addfec978708270f9526dabe080
Tony Faller <amfaller@bu.edu>
2023-10-10 17:35:25 -0400

    Define Server class

--- include/Server.h
+++ include/Server.h
@@ -5,3 +5,32 @@
 // service we're implementing.
 //
 ///////////////////////////////////////////////////////////////////////////////
+
+#ifndef SERVER_H
+#define SERVER_H
+
+// This is the only include needed for crow
+#include "crow.h"
+
+// Translator header
+#include "../include/Translator.h"
+
+class Server
+{
+public:
+    // ctor
+    Server();
+
+    // Function to start the server
+    void run();
+
+private:
+    // The app itself
+    crow::SimpleApp app;
+
+    // Translator object to do translations
+    Translator translator;
+    
+};
+
+#endif // SERVER_H
--- src/Server.cpp
+++ src/Server.cpp
@@ -5,3 +5,52 @@
 // service we're implementing.
 //
 ///////////////////////////////////////////////////////////////////////////////
+
+#include "../include/Server.h"
+
+// ctor
+Server::Server()
+    : app(),
+      translator()
+{
+    // Define the translation route
+    CROW_ROUTE(app,"/translate/")([this](const crow::request& req, crow::response& res){
+        // Access query string parameters using crow::request::url_params
+        auto textToBeTranslated = req.url_params.get("tbt");
+        auto toLang = req.url_params.get("tl");
+        auto fromLang = req.url_params.get("fl");
+
+        // Check if parameters exist and handle accordingly
+        if (textToBeTranslated && toLang && fromLang)
+        {
+            // Output
+            std::string translatedText;
+            translator.doTranslation(translatedText, textToBeTranslated, toLang, fromLang);
+
+            // The rest of this IF block is for debugging
+            std::ostringstream oss;
+            oss << "textToBeTranslated: " << textToBeTranslated
+                << ", toLang: " << toLang
+                << ", fromLang: " << fromLang
+                << std::endl
+                << "Translated text: " << translatedText
+                << std::endl;
+                
+            res.set_header("Content-Type", "text/plain");
+            res.write(oss.str());
+            res.end();
+        }
+        else
+        {
+            res.code = 400; // Bad Request
+            res.write("Missing query parameters.\n");
+            res.end();
+        }
+    });
+}
+
+// Function to start the server
+void Server::run()
+{
+    app.port(18080).multithreaded().run();
+}
