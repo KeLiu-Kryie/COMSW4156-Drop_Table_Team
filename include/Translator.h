0789ceaaf0af948674831ae0a446f28aaf746d61
Tony Faller <amfaller@bu.edu>
2023-10-10 17:02:57 -0400

    Define Translator class

--- include/Translator.h
+++ include/Translator.h
@@ -9,6 +9,41 @@
 #ifndef TRANSLATOR_H
 #define TRANSLATOR_H
 
+#include <curl/curl.h>
+#include <iostream>
+#include <openssl/md5.h>
+#include <sstream>
+#include <string>
+#include <cstring>
 
+class Translator
+{
+public:
+    // ctor
+    Translator();
+
+    // dtor
+    ~Translator();
+
+    // Function to perform the translation
+    bool doTranslation(std::string& translatedText, // output
+                       std::string textToBeTranslated,
+                       std::string fromLang,
+                       std::string toLang) const;
+
+private:
+    // Fundamental curl pointer
+    CURL *curl;
+
+    // REST endpoint
+    std::string url;
+
+    // Not exactly sure what this field is for.
+    // @Ke -- any idea?
+    std::string appId;
+
+    // API Key
+    std::string key;
+};
 
 #endif // TRANSLATOR_H
