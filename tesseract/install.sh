0ed42a9fac513a922b0f6be37ddbe9e82c3b842a
brendan <brendan.fay@columbia.edu>
2023-10-12 14:07:54 -0400

    undoing this

--- tesseract/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-sudo apt install tesseract-ocr libtesseract-dev g++ autoconf automake libtool pkg-config libpng-dev libjpeg8-dev libtiff5-dev zlib1g-dev libwebpdemux2 libwebp-dev libopenjp2-7-dev libgif-dev libarchive-dev libcurl4-openssl-dev libleptonica-dev
-
-
-
